34ea29a0be5374c7d44c630af828520615c0ce04
abirajs <novalnetchennai@gmail.com>
2023-06-07 16:25:26 +0530

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -143,6 +143,11 @@ class WebhookController extends Controller
      */
     public function processWebhook()
     {
+        // Validate the event procedures 
+		if ( !empty( $this->event_data ['custom'] ['shop_invoked'] ) ) {
+          return  $this->display_message( array( 'message' => 'Process already handled in the shop.' ) );
+        }
+        
         // validated the IP Address
         $invalidIpMsg =  $this->validateIpAddress();
         if(!empty($invalidIpMsg)) {

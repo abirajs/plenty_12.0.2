db955db322c52d935da78885effecfe1f78e6668
abirajs <novalnetchennai@gmail.com>
2023-06-08 18:02:27 +0530

    Update novalnetApplePay.js

--- resources/js/novalnetApplePay.js
+++ resources/js/novalnetApplePay.js
@@ -1,14 +1,12 @@
 jQuery(document).ready(function () {
 	
 	var mopId = jQuery('#nn_apple_pay_mop').val();
-	alert(mopId);
 	 jQuery('li[data-id="'+mopId+'"]').click(function() {
 		alert('Not Supported');
 	});
 	
 	 jQuery('.method-list-item').on('click',function() {
                 var clickedId = jQuery(this).attr('data-id');
-		 alert(clickedId);
                 if(clickedId !== undefined && clickedId != mopId) {
                   alert('NO');
                } else {
